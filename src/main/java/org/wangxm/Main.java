e45eb4ec69650934886641b926928e4697617931
DESKTOP-32LMAQK\wangxm <markwangxm@163.com>
2022-05-06 23:02:25 +0800

    111222

--- src/main/java/org/wangxm/Main.java
+++ src/main/java/org/wangxm/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         User user = (User) applicationContext.getBean("wangxm");
-        System.out.println("id=>" + user.getId());
-        System.out.println("userName=>" + user.getUserName());
-        System.out.println("email=>" + user.getEmail());
+        System.out.println("id => " + user.getId());
+        System.out.println("userName => " + user.getUserName());
+        System.out.println("email => " + user.getEmail());
     }
 }
